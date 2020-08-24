0dc4a44e96fe0d36be53eff31678624a2bd1800a
Avanthi <avanthi.divvela@gmail.com>
2020-08-24 13:34:12 -0500

    console app1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Datatypes
+            //string firstName = "Avanthi";
+            //int age = 34;
+            //float exactAge = 34.2f;
+            //char charValue = 'c';
+            //bool isActive = true;
+
+            ////conditional statements
+            //if(age >35)
+            //{
+            //    Console.WriteLine(firstName + "'s age is greaterthan 35yrs");
+            //}
+            //else if(age == 34)
+            //{
+            //    Console.WriteLine(firstName + "'s age is 30");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(firstName + "'s age is lessthan 35yrs");
+            //}
+
+
+            //**************Exception Handling ******************
+            //try
+            //{
+            //    int i = 0;
+            //    int j = 10;
+            //    Console.WriteLine(j / i);              
+            //}
+            //catch(DivideByZeroException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+
+            //}
+            //Console.ReadLine();
+
+            //*************Access Modifiers*****************
+
+            AccessModifiers obj_AccessModifiers = new AccessModifiers();
+            obj_AccessModifiers.GetEmpName();
+
+
+        }
+    }
+}
